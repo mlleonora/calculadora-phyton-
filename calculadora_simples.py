96085e7e32009f4e062d6d433d004058029291fb
mlleonora <105123823+mlleonora@users.noreply.github.com>
2022-07-21 23:30:03 -0300

    Add files via upload

---
+++ calculadora_simples.py
@@ -0,0 +1,14 @@
+from ast import match_case
+from unittest import case
+num1 = int(input("enter num1:"))
+num2 = int(input("enter num2:"))
+match_case = str(input("Choose action: Add(a), Sub(s), Mult(m), Div(d)->>"))
+print("The result is", end="")
+case = "a";
+print(num1+num2)
+case ="s";
+print(num1-num2)
+case ="m";
+print(num1*num2);
+case
+print(num1/num2)
